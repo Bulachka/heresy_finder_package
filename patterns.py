5fdf180c65a9f70aad6d57382fe4463ef2a3ca7b
Bulachka <bella-divina@yandex.by>
2025-04-19 15:52:21 +0200

    debug pattern slash

--- patterns.py
+++ patterns.py
@@ -2,6 +2,6 @@
 raw_patterns = [
     r"chetzer.*", r"kætzer.*", r"kätzer.*", r"keczer.*", r"keczir.*", r"keczzer.*", r"keczczer.*",
     r"ketzcer.*", r"ketzcir.*", r"ketzeer.*", r"ketzer.*", r"khetzer.*", r"ketczer.*",
-    r"haeres\\w+", r"haeret.*", r"heret.*", r"heres\\w+", r"hæres\\w+", r"hæret.*",
+    r"haeres\w+", r"haeret.*", r"heret.*", r"heres\w+", r"hæres\w+", r"hæret.*",
     r"huss.*"
 ]
