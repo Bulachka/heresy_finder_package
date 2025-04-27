22e2c027ab0e6495c9ff827a93cde4166ba9093c
Bulachka <bella-divina@yandex.by>
2025-04-27 20:26:01 +0200

    add ketczir pattern

--- patterns.py
+++ patterns.py
@@ -2,6 +2,7 @@
 raw_patterns = [
     r"chetzer.*", r"kætzer.*", r"kätzer.*", r"keczer.*", r"keczir.*", r"keczzer.*", r"keczczer.*",
     r"ketzcer.*", r"ketzcir.*", r"ketzeer.*", r"ketzer.*", r"khetzer.*", r"ketczer.*", r"kettzer.*",
+    r"ketczir.*",
     r"haeres\w+", r"haeret.*", r"heret.*", r"heres\w+", r"hæres\w+", r"hæret.*",
     r"huss.*"
 ]
